f74454fb8ddd535997b8dbe1322a292bee0d6993
inazputri <nzkmila@gmail.com>
2025-11-28 22:31:03 +0800

    coba2

---
+++ Node.java
@@ -0,0 +1,12 @@
+class Node{
+    String buku;
+    int halaman;
+    Node next;
+
+    Node(String buku, int halaman){
+        this.buku = null;
+        this.halaman = null;
+        this.next = null;
+
+    }
+}
